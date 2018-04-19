50c5327bbdb1978967c6e9fc38aadf36dbafa0f3
Evgenii <zlvlezhnin@gmail.com>
2018-04-19 12:04:26 +0300

    introduce simple java app

---
+++ src/main/java/com/db/edu/LoggerController.java
@@ -0,0 +1,7 @@
+package com.db.edu;
+
+public class LoggerController {
+    public void log(String message) {
+        System.out.println(message);
+    }
+}
---
+++ src/test/java/LoggerTest.java
@@ -0,0 +1,8 @@
+import com.db.edu.LoggerController;
+
+public class LoggerTest {
+    public static void main(String[] args) {
+        LoggerController controller = new LoggerController();
+        controller.log("test string");
+    }
+}
